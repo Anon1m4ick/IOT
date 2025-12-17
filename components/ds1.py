444d3cfbf458756dd1e0c85f0fac968ca27b0c0d
Anon1m4ick <vova220520022145432@gmail.com>
2025-12-17 19:41:12 +0100

    add dpir1 for components/simulators

--- components/ds1.py
+++ components/ds1.py
@@ -1,7 +1,10 @@
 import threading
 import time
+# from settings import load_settings
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/..')
 from settings import load_settings
-
 from simulators.ds1 import run_ds1_simulator
 
 def ds1_callback(message):
@@ -13,7 +16,7 @@ def ds1_callback(message):
 def run_ds1(settings, threads, stop_event):
     if settings['simulated']:
         print("Starting ds1 simulator")
-        ds1_thread = threading.Thread(target=run_ds1_simulator, args=(0.5, ds1_callback, stop_event))
+        ds1_thread = threading.Thread(target=run_ds1_simulator, args=(ds1_callback, stop_event))
         ds1_thread.start()
         threads.append(ds1_thread)
         print("Ds1 simulator started")
@@ -26,8 +29,8 @@ def run_ds1(settings, threads, stop_event):
         threads.append(ds1_thread)
         print("Ds1 loop started")
 
-# settings = load_settings()
-# ds1_settings = settings['DS1']
-# threads = []
-# stop_event = threading.Event()
-# run_ds1(ds1_settings, threads, stop_event)
+settings = load_settings()
+ds1_settings = settings['DS1']
+threads = []
+stop_event = threading.Event()
+run_ds1(ds1_settings, threads, stop_event)
