00abc27efde9cf2e0a98bf95cadf66d1b3fe9381
wbod_k <minakoffroman2015@gmail.com>
2025-12-21 17:13:35 +0100

    door light simulation

---
+++ src/simulators/dl.py
@@ -0,0 +1,24 @@
+import time
+import random
+
+def run_dl_simulator(callback, stop_event):
+    state = 0
+    while not stop_event.is_set():
+        time.sleep(0.1)
+    callback("LED Simulator stopped")
+
+def set_dl_state(state: int):
+    global _dl_state
+    _dl_state = state
+    return state
+
+def get_dl_state():
+    global _dl_state
+    try:
+        return _dl_state
+    except NameError:
+        _dl_state = 0
+        return 0
+
+_dl_state = 0
+
